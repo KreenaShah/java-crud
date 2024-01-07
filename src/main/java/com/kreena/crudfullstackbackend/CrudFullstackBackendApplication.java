1c655cd2ade2a63330da9736bb3e1f979a2a4c88
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2024-01-07 10:23:55 +0530

    api implemented

---
+++ src/main/java/com/kreena/crudfullstackbackend/CrudFullstackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kreena.crudfullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFullstackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kreena/crudfullstackbackend/CrudFullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreena.crudfullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
